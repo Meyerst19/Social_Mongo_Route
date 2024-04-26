a81dcab1d683043390de024e5d249c5fc701b615
TME <tyler.s.meyers@gmail.com>
2024-04-26 15:11:09 -0600

    validator on email and date get format done

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,4 +1,22 @@
 const { Schema, Types } = require("mongoose");
+function formatDate(dbDate) {
+  const date = new Date(dbDate);
+  const d = date.getDate();
+  const m = date.getMonth() + 1;
+  const y = date.getFullYear();
+  const h = date.getHours();
+  const min = date.getMinutes();
+
+  if (h === 0) {
+    return `${m}/${d}/${y} @ ${h + 1}:${min} AM`;
+  } else if (h === 12) {
+    return `${m}/${d}/${y} @ ${h}:${min} PM`;
+  } else if (h > 12) {
+    return `${m}/${d}/${y} @ ${h - 12}:${min} PM`;
+  } else {
+    return `${m}/${d}/${y} @ ${h}:${min} AM`;
+  }
+}
 
 const reactionSchema = new Schema(
   {
@@ -18,6 +36,7 @@ const reactionSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
+      get: formatDate,
     },
   },
   {
--- models/Thought.js
+++ models/Thought.js
@@ -2,6 +2,25 @@ const { Schema, model } = require("mongoose");
 const Reaction = require("./Reaction");
 const { ObjectId } = require("mongoose").Types;
 
+function formatDate(dbDate) {
+  const date = new Date(dbDate);
+  const d = date.getDate();
+  const m = date.getMonth() + 1;
+  const y = date.getFullYear();
+  const h = date.getHours();
+  const min = date.getMinutes();
+
+  if (h === 0) {
+    return `${m}/${d}/${y} @ ${h + 1}:${min} AM`;
+  } else if (h === 12) {
+    return `${m}/${d}/${y} @ ${h}:${min} PM`;
+  } else if (h > 12) {
+    return `${m}/${d}/${y} @ ${h - 12}:${min} PM`;
+  } else {
+    return `${m}/${d}/${y} @ ${h}:${min} AM`;
+  }
+}
+
 const thoughtSchema = new Schema(
   {
     thoughtText: {
@@ -13,6 +32,7 @@ const thoughtSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
+      get: formatDate,
     },
     username: {
       type: String,
@@ -23,22 +43,23 @@ const thoughtSchema = new Schema(
   {
     toJSON: {
       virtuals: true,
+      getters: true,
     },
     id: false,
   }
 );
 
-thoughtSchema.methods.removeReactionId = function (id) {
-  const remainingReactions = [];
-  for (i = 0; i < this.reactions.length; i++) {
-    if (this.reactions[i]._id === id) {
-      remainingReactions;
-    } else {
-      remainingReactions.push(this.reactions[i]);
-    }
-  }
-  return remainingReactions;
-};
+// thoughtSchema.methods.removeReactionId = function (id) {
+//   const remainingReactions = [];
+//   for (i = 0; i < this.reactions.length; i++) {
+//     if (this.reactions[i]._id === id) {
+//       remainingReactions;
+//     } else {
+//       remainingReactions.push(this.reactions[i]);
+//     }
+//   }
+//   return remainingReactions;
+// };
 
 thoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length;
